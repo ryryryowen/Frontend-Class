84d721646561ddd1fbc1b7bb47f29a0998757c1d
MacUser <macuser@MacUserui-MacBookPro.local>
2024-07-02 17:36:27 +0900

    240702

---
+++ 240620/script.js
@@ -0,0 +1,43 @@
+//scroll event
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  const nav = document.querySelector("nav");
+  const logo = document.querySelector("#logo");
+  const gnbDesktop = document.querySelector(".gnbDesktop");
+  const gnbMobile = document.querySelector(".gnbMobile");
+  const trigger = document.querySelectorAll(".trigger span")
+
+  if (window.scrolly > 60) {
+    header.classList.add("active");
+    nav.classList.add("active");
+    logo.classList.add("active");
+    gnbDesktop.classList.add("active");
+    gnbMobile.classList.add("active");
+    trigger.forEach((bar) => {
+      bar.classList.add("active");
+    });
+  } else {
+    header.classList.remove("active");
+    nav.classList.remove("active");
+    logo.classList.remove("active");
+    gnbDesktop.classList.remove("active");
+    gnbMobile.classList.remove("active");
+      trigger.forEach((bar) => {
+        bar.classList.remove("active");
+      });
+  }
+});
+
+// toggle event
+const toggleBtn = document.querySelector(".trigger");
+toggleBtn.addEventListener("click", function () {
+  const gnbMobile = document.querySelector(".gnbMobile");
+  gnbMobile.classList.toggle("open");
+  this.classList.toggle("active");
+});
+
+// serachbar trigger
+const serachbar = document.querySelectorAll("ul .fa-magnifying-glass");
+const serachResult = document.querySelector(".search_bar");
+const closeBtn = document.querySelector(".fa-xmark");
+
