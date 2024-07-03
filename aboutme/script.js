0219a8f566c03a6894565fd36458b6c12ff8f895
MacUser <macuser@MacUserui-MacBookPro.local>
2024-07-03 18:25:00 +0900

    240703

--- aboutme/script.js
+++
@@ -1,9 +0,0 @@
-//  Navbar Toggle Btn
-const toggleBtn = document.querySelector(".navbar_toogleBtn");
-const menu = document.querySelector(".navbar_menu");
-const code = document.querySelector(".navbar_code");
-
-toggleBtn.addEventListener("click", () => {
-  menu.classList.toggle("active");
-  code.classList.toggle("active");
-});
