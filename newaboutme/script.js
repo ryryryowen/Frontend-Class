1e3b3766db19a7f71f27675927fe0b9b95a9ec51
ryryryowen <dhfma0713@gmail.com>
2024-07-05 19:23:32 +0900

    240705.2

--- newaboutme/script.js
+++ newaboutme/script.js
@@ -1,5 +1,17 @@
+// 로딩창
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    document.querySelectorAll(".loader").forEach(function (loader) {
+      loader.style.display = "none";
+    });
+    document.body.style.background = "white"; // 배경색 변경
+    document.getElementById("main-content").style.display = "block";
+  }, 2000); // 2초
+});
+
 // typing dashborad
-const content = "안녕하세요!✋\n프론트엔드 개발을 배우고있는\n 코딩 왕초보 해오름입니다 \n:D";
+const content =
+  "안녕하세요!✋\n프론트엔드 개발을 배우고있는\n 코딩 왕초보 해오름입니다 \n:D";
 
 const text = document.querySelector(".text");
 const cursor = document.querySelector(".blink");
@@ -7,7 +19,6 @@ const cursor = document.querySelector(".blink");
 let i = 0;
 
 const typing = () => {
-
   if (i < content.length) {
     let txt = content[i++];
     text.innerHTML += txt === "\n" ? "<br/>" : txt;
@@ -18,5 +29,20 @@ const typing = () => {
   }
 };
 
-
 typing();
+
+// 스크롤바
+let scrollTop = 0;
+let bar = document.getElementsByClassName("bar-ing")[0];
+
+window.addEventListener(
+  "scroll",
+  () => {
+    scrollTop = document.documentElement.scrollTop;
+    let per = Math.ceil(
+      (scrollTop / (document.body.scrollHeight - window.outerHeight)) * 100
+    );
+    bar.style.width = per + "%";
+  },
+  false
+);
