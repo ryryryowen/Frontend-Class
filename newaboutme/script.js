4f4a20eb39ea89e941c23beb13951ae07c5cff9d
ryryryowen <dhfma0713@gmail.com>
2024-07-09 18:28:11 +0900

    240709

--- newaboutme/script.js
+++ newaboutme/script.js
@@ -47,48 +47,6 @@ window.addEventListener(
   false
 );
 
-// about 슬라이드 이미지
-// 이미지 슬라이드
-let imgIndex = 0;
-let position = 0;
-const IMG_WIDTH = 438;
-const $btnPrev = document.querySelector(".btn-prev");
-const $btnNext = document.querySelector(".btn-next");
-const $slideImgs = document.querySelector(".slide-images");
-
-let prev = function () {
-  if (imgIndex > 0) {
-    $btnNext.removeAttribute("disabled");
-    position += IMG_WIDTH;
-    $slideImgs.style.transform = `translateX(${position}px)`;
-    imgIndex = imgIndex - 1;
-  }
-  if (imgIndex == 0) {
-    $btnPrev.setAttribute("disabled", "true");
-  }
-};
-
-let next = function () {
-  if (imgIndex < 5) {
-    $btnPrev.removeAttribute("disabled");
-    position -= IMG_WIDTH;
-    $slideImgs.style.transform = `translateX(${position}px)`;
-    $slideImgs.style.transition = "transform .5s ease-out";
-    imgIndex = imgIndex + 1;
-  }
-  if (imgIndex == 4) {
-    $btnNext.setAttribute("disabled", "true");
-  }
-};
-
-let init = function () {
-  $btnPrev.setAttribute("disabled", "true");
-  $btnPrev.addEventListener("click", prev);
-  $btnNext.addEventListener("click", next);
-};
-
-init();
-
 // 모달창
 
 const btn = document.querySelector("#open");
