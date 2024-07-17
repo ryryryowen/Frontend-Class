854db9bd755e33d1516003ec41bbfbc1db89d207
ryryryowen <dhfma0713@gmail.com>
2024-07-17 16:51:08 +0900

    240717

--- 240715/02flex/script.js
+++ 240715/02flex/script.js
@@ -2,12 +2,32 @@
 window.addEventListener("scroll", () => {
   let scroll = window.scrollY;
   const header = document.querySelector("header");
+  const gototop = document.querySelector(".gototop");
 
   if (scroll > 50) {
     header.classList.add("active");
+    gototop.classList.add("active");
   } else {
     header.classList.remove("active");
+    gototop.classList.remove("active");
   }
+});
+
+// Trigger
+const trigger = document.querySelector(".trigger");
+const gnb = document.querySelector(".gnb");
+const gnbLinks = document.querySelectorAll("a");
+
+trigger.addEventListener("click", function () {
+  this.classList.toggle("active");
+  gnb.classList.toggle("active");
+});
+
+gnbLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    trigger.classList.remove("active");
+    gnb.classList.remove("active");
+  });
 });
 
 // Slick Slider
