f6a23d9bdd5f0a0a940045378eae77523e6d67bc
ryryryowen <dhfma0713@gmail.com>
2024-07-29 17:47:37 +0900

    240729

---
+++ 240729/01js/script.js
@@ -0,0 +1,54 @@
+// const test1 = "mon";
+// test1 = "tue";
+
+//  프로그램을 개발
+//  - width 1920px
+//  - 슬라이드 개수 5개 고정
+
+// 스크롤을 내리게 되었을 때, 내려간 값에 따라서 무언가를 변경 !!
+
+// let test2 = "mon";
+
+// test2 = "tue";
+
+//  조별 프로젝트
+
+// A
+// - 카테고리 안에 상품개수 10개
+// var cartegory = 10;
+
+// B
+//  장바구니에 담긴 상품개수
+// var cartegory = 10;
+
+//  병합
+
+// var x = 10;
+
+// var z = x + y;
+
+// var y = 20;
+// console.log(z);
+
+//  Not a Number => 자료의 형태 중 하나 !!!
+
+//
+
+// const srt1 = "1";
+// const srt2 = "2";
+
+// console.log(typeof srt1);
+
+const num = 1000;
+const str = "원";
+
+const counter = num + str;
+console.log(typeof counter);
+
+const num1 = 1000;
+const str1 = "2000";
+
+const counter1 = num1 + str1;
+console.log(counter1);
+
+prompt("얼마를 원하십니까?");
