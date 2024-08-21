45700c05242fa2ebc683172ace2958cd68816af9
MacUser <macuser@MacUserui-MacBookPro.local>
2024-08-21 21:45:28 +0900

    240821

--- 240816/07js/script.js
+++ 240816/07js/script.js
@@ -1,69 +1,65 @@
 const playButton = document.querySelector(".play-pause");
 const video = document.querySelector("video");
-const volumeBar = document.querySelector("input[type='range']");
+const volumeBar = document.querySelector("input[type = 'range']");
 const progressCover = document.querySelector(".progress");
 const player = document.querySelector(".player");
+const rateButtons = document.querySelectorAll(".rate");
+const fullButton = document.querySelector("#fullscreenBtn");
 
 const play = () => {
-  playButton.innerText = "||";
+  playButton.innerText = "| |";
   video.play();
 };
-
 const pause = () => {
-  playButton.innerText = "▶️";
+  playButton.innerText = ":앞쪽_화살표:";
   video.pause();
 };
-
+video.addEventListener("ended", pause);
 const togglePlay = () => {
   video.paused ? play() : pause();
 };
-
 const setVolume = (e) => {
   video.volume = e.target.value;
 };
-
 const formatting = (time) => {
   let sec = Math.floor(time % 60);
   let min = Math.floor(time / 60);
   let hour = Math.floor(time / 3600);
-
-  sec = sec < 10 ? `0${sec}` : sec;
-  min = min < 10 ? `0${min}` : min;
-  hour = hour < 10 ? `0${hour}` : hour;
-
+  sec < 10 ? (sec = `0${sec}`) : sec;
+  min < 10 ? (min = `0${min}`) : min;
+  hour < 10 ? (hour = `0${hour}`) : hour;
   return `${hour}:${min}:${sec}`;
 };
-
 const updateTime = () => {
   const current = document.querySelector(".current");
-  const duration = document.querySelector(".duration");
-
   current.innerText = formatting(video.currentTime);
+  const duration = document.querySelector(".duration");
   duration.innerText = formatting(video.duration);
 };
-
 const updateProgress = () => {
   const progressBar = document.querySelector(".bar");
   const progressPointer = document.querySelector(".circle");
   const duration = video.duration;
   const currentTime = video.currentTime;
-  const percent = (currentTime / duration) * 100;
-  progressBar.style.width = `${percent}%`;
+  const percentage = (currentTime / duration) * 100;
+  progressBar.style.width = `${percentage}%`;
   const progressBarWidth = progressCover.clientWidth;
-  const newPosition = (currentTime / duration) * progressBarWidth;
+  const newPosition = (currentTime / duration) * progressBarWidth - 1;
   progressPointer.style.left = `${newPosition}px`;
+  console.log(progressBarWidth);
 };
-
 const videoPoint = (e) => {
-  console.log(e.pageX);
-  console.log(player.offsetLeft);
-  const mouseX = e.pageX - player.offsetLeft;
   const progressBarWidth = progressCover.clientWidth;
   const duration = video.duration;
-  const clickedTime = (mouseX / progressBarWidth) * duration;
+  const pointerX = e.pageX - player.offsetLeft;
+  const clickedTime = (pointerX / progressBarWidth) * duration;
   video.currentTime = clickedTime;
 };
-
+const setRate = (e) => {
+  const { rate } = e.target.dataset;
+  console.log(rate);
+  video.playbackRate = rate;
+};
 playButton.addEventListener("click", togglePlay);
 video.addEventListener("click", togglePlay);
 video.addEventListener("timeupdate", updateTime);
@@ -71,6 +67,16 @@ video.addEventListener("timeupdate", updateProgress);
 volumeBar.addEventListener("change", setVolume);
 progressCover.addEventListener("click", (e) => {
   videoPoint(e);
+});
+rateButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    setRate(e);
+  });
+});
+
+// Fullscreen
+fullButton.addEventListener("click", () => {
+  video.requestFullscreen();
 });
 
 // 1. 브라우저 창을 기준으로 좌.우.상.하 측면에서 얼만큼 떨어졌는가를 알 수 있게 해주는 속성
